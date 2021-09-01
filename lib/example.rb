be47a37057e8100be1ebb13a89e9a380c8f98f62
Allen Mathews <allenjoemathews@gmail.com>
2021-09-02 08:03:31 +1000

    commit tree c8517c1dc60d5fa8ce169b4401a3f77b481b91ac

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,98 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+# tree_name = "pine"
+# $car_name = "Peugeot"
+# @sea_name = "Black sea"
+
+# class Animal
+#     @@species = "Cat"
+# end
+
+# p local_variables
+# p global_variables.include? :$car_name
+# p self.instance_variables
+# p Animal.class_variables
+
+# def method1
+#     x = 5
+#     p x
+#  end
+ 
+#  method1
+ 
+#  p x
+
+# x = 5
+
+# def method1
+#     x = 10
+#     p x
+# end
+
+# method1
+
+# p x
+
+# def rectangle_area a, b
+
+#     puts local_variables
+#     return a * b
+# end
+
+# puts rectangle_area 5, 6
+
+
+# def method1
+
+#     def method2
+
+#         def method3
+
+#             m5, m6 = 3
+#             puts "Level 3"
+#             puts local_variables
+#         end
+
+#         m3, m4 = 3
+#         puts "Level 2"
+#         puts local_variables
+#         method3
+#     end
+
+#     m1, m2 = 3
+#     puts "Level 1"
+#     puts local_variables
+#     method2
+
+# end
+
+# method1
+# module ModuleM
+#     m1, m2 = 4
+
+#     puts "Inside module"
+#     puts local_variables
+# end
+
+
+# def method1
+#     v, w = 3
+#     puts "Inside method"
+#     puts local_variables
+# end
+
+
+# class Some
+#     x, y = 2
+#     puts "Inside class"
+#     puts local_variables
+# end
+
+# method1
+
+# t1, t2 = 7
+
+# puts "Inside toplevel"
+# puts local_variables
